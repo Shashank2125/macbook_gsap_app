2ff98ba5edf32f82b6eab5e98cda5d7905e487db
Shashank2125 <138694950+Shashank2125@users.noreply.github.com>
2026-01-05 11:40:19 +0530

    fixed showcase

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
 import ProductViewer from "./components/ProductViewer.jsx";
 import gsap from'gsap';
-import {ScrollTrigger} from "gsap/all";
+import { ScrollTrigger } from "gsap/all";
 import Showcase from "./components/Showcase.jsx";
 
 gsap.registerPlugin(ScrollTrigger);
@@ -11,12 +11,13 @@ const App = () => {
     return (
 
 
-       <main>
-           <NavBar />
-           <Hero/>
-           <ProductViewer/>
-           <Showcase/>
-       </main>
+        <main>
+            <NavBar />
+            <Hero/>
+            <ProductViewer/>
+            <Showcase/>
+        </main>
     )
 }
 export default App
+
