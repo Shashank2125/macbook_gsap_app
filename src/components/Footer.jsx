3d7b8922586824d522fe568b11781085ac6cdd5b
Shashank2125 <138694950+Shashank2125@users.noreply.github.com>
2026-01-08 16:37:14 +0530

    footer implementation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,27 @@
+import {footerLinks} from "../constants/index.js";
 
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <div className='info'>
+                <p>More ways to shop: Find an Apple Store or other retailer near you. or call 00800 040 1966</p>
+                <img src="/logo.svg" alt="Apple logo"/>
+            </div>
+
+            <hr/>
+            <div className="links">
+                <p> copyright © 2024 Apple Inc. All rights reserved.</p>
+                <ul>
+                    {footerLinks.map(({label,link}) => (
+                        <li key={label}>
+                            <a href={link}>{label}</a>
+                        </li>
+                    ))}
+                </ul>
+
+            </div>
+        </footer>
     )
 }
 export default Footer
